e14499e621b92f14eb1e60166bfbc269131fbca0
Tianhang Liu <tianhanl@usc.edu>
2018-04-28 20:59:15 -0700

    add proc

--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,24 @@ Route::post('/signup', 'SignupController@signup');
 Route::get('/login', 'LoginController@index');
 Route::post('/login', 'LoginController@login');
 Route::get('/logout', 'LoginController@logout');
-Route::get('/items/new', 'ItemsController@create');
-Route::get('/items/{id}/edit', 'ItemsController@edit');
-Route::get('/items/{id}/delete', 'ItemsController@destroy');
-Route::get('/items/{id}', 'ItemsController@show');
-Route::post('/items/{id}', 'ItemsController@update');
-Route::get('/items', 'ItemsController@index');
-Route::post('/items', 'ItemsController@store');
-Route::get('/transactions/{id}/edit', 'TransactionController@edit');
-Route::get('/transactions/{id}/delete', 'TransactionController@destroy');
-Route::get('/transactions', 'TransactionController@index');
-Route::get('/transactions', 'TransactionController@index');
-Route::post('/transactions', 'TransactionController@create');
-Route::get('/profile', 'AdminController@index');
+
 
 Route::middleware(['protected'])->group(function () {
     Route::get('/profile', 'AdminController@index');
     Route::get('/phpinfo', function () {
         echo phpinfo();
     });
+    Route::get('/items/new', 'ItemsController@create');
+    Route::get('/items/{id}/edit', 'ItemsController@edit');
+    Route::get('/items/{id}/delete', 'ItemsController@destroy');
+    Route::get('/items/{id}', 'ItemsController@show');
+    Route::post('/items/{id}', 'ItemsController@update');
+    Route::get('/items', 'ItemsController@index');
+    Route::post('/items', 'ItemsController@store');
+    Route::get('/transactions/{id}/edit', 'TransactionController@edit');
+    Route::get('/transactions/{id}/delete', 'TransactionController@destroy');
+    Route::get('/transactions', 'TransactionController@index');
+    Route::get('/transactions', 'TransactionController@index');
+    Route::post('/transactions', 'TransactionController@create');
+    Route::get('/profile', 'AdminController@index');
 });
